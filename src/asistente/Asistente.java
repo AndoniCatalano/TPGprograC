c2fbe293fbe22e4833cd635723044206bd0c25c5
mastakastomas-dot <mastakastomas@gmail.com>
2026-09-13 16:51:47 -0300

    estructura inicial de los metodos

---
+++ src/asistente/Asistente.java
@@ -0,0 +1,62 @@
+public class Asistente {
+    /*
+    Mapa mapa
+    Mision mision
+
+    preparar()
+    ejecutar()
+    evaluarResultado()
+    cerrar()
+    * */
+    Nave nave;
+
+
+    public double consultarEnergia(){
+
+    }
+
+    public double consultarCombustible(){
+
+    }
+
+    public double consultarDesgaste(){
+
+    }
+
+   /*
+    public void prepararMision(Mision mision){
+        //
+    }
+
+    public void ejecutarMision(Mision mision){
+
+    }
+
+    public void iniciarMision(Mision mision){
+
+    }
+    */
+
+    public Informe crearInforme(){
+
+    }
+
+    //le pasa el informe a la nave para que lo guarde en la bitacora
+    public void entregaInforme(){
+
+    }
+
+    public void ordenCargaCombustible(double cantidad){
+
+    }
+
+    public void ordenCargaEnergia(double cantidad){
+
+    }
+
+    //al realizar el mantenimiento reseteamos a 0 el desgaste o hacemos que reste cierta cantidad?
+    public void ordenMantenimientoNave(){
+
+    }
+
+}
