07f9c57b83bc109ec612fcd669553e577ae2bae4
mastakastomas-dot <mastakastomas@gmail.com>
2026-09-16 19:48:05 -0300

    se agrego el informe

---
+++ src/asistente/Informe.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Informe {
+    private Mision mision_ejecutada;
+    private String resultado;
+    private List<String> accionesRealizadas;
+    private int combustibleCons;
+    private int energiaCons;
+    private int estadoFinal;
+    private Lista<String> observaciones;
+
+}
