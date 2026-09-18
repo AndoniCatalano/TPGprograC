e1f12223a93171d1e7491a9b25b0fe733f02ff31
mastakastomas-dot <mastakastomas@gmail.com>
2026-09-18 15:50:19 -0300

    se agregaron returns y llamados a metodos de nave

--- src/asistente/Asistente.java
+++ src/asistente/Asistente.java
@@ -8,24 +8,49 @@ public class Asistente {
     evaluarResultado()
     cerrar()
     * */
+    Mision mision;
     Nave nave;
+    NaveFactory fabrica = new NaveFactory();
 
+    public Asistente(){
+
+    }
+
+
+
+    public void creaNave(String tipoNave){
+        nave = fabrica.getNave(tipoNave);
+    }
 
     public double consultarEnergia(){
-
+        return this.nave.getEnergia();
     }
 
     public double consultarCombustible(){
-
+        return this.nave.getCombustible();
     }
 
     public double consultarDesgaste(){
-
+        return this.nave.getDesgaste();
     }
+    public void ordenCargaCombustible(double cantidad){
+        this.nave.cargaComubstible(cantidad);
+    }
+
+    public void ordenCargaEnergia(double cantidad){
+        this.nave.cargaEnergia(cantidad);
+    }
+
+    //al realizar el mantenimiento reseteamos a 0 el desgaste o hacemos que reste cierta cantidad?
+    public void ordenMantenimientoNave(){
+        this.nave.realizarMantenimiento();
+    }
+
+
 
    /*
     public void prepararMision(Mision mision){
-        //
+
     }
 
     public void ejecutarMision(Mision mision){
@@ -37,26 +62,17 @@ public class Asistente {
     }
     */
 
-    public Informe crearInforme(){
+
+
+    public void actualizaInforme(Informe informe){
 
     }
 
-    //le pasa el informe a la nave para que lo guarde en la bitacora
-    public void entregaInforme(){
 
+    public Informe entregaInforme(Informe inf){
+        return inf;
     }
 
-    public void ordenCargaCombustible(double cantidad){
 
-    }
-
-    public void ordenCargaEnergia(double cantidad){
-
-    }
-
-    //al realizar el mantenimiento reseteamos a 0 el desgaste o hacemos que reste cierta cantidad?
-    public void ordenMantenimientoNave(){
-
-    }
 
 }
