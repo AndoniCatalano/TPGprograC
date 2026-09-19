6841687ef8652add0cd508308f30c723c173a3bd
SofiCasais <50109428+SofiCasais@users.noreply.github.com>
2026-09-19 10:06:09 -0300

    Add files via upload

--- Nave.java
+++ Nave.java
@@ -2,13 +2,23 @@ public interface Nave { //contiene los métodos comunes a todas las naves
     public void cargaCombustible(int combustible);
     public void cargaEnergia(int energia);
     public void realizarMantenimiento();
+    public void consumeCombustible(int cantCombustible);
+    public void consumeEnergia(int cantEnergia);
+    public void incrementaDesgaste(int cantDesgaste);
+    public double liquidarHaberes();
+
+
+    public void insertaTripulante(Tripulante tripulante);
+    public void insertaMision(Mision mision);
+    public void insertaBitacora(Bitacora bitacora);
 
     public int getCombustible();
     public int getEnergia();
     public int getDesgaste();
     public boolean getNecesitaMantenimiento();
     public Motor getMotorWarp();
-    public Tripulacion[] getArrayTripulacion();
+    public Tripulante[] getArrayTripulantes();
     public Mision[] getArrayInformes();
+    public Bitacora[] getArrayBitacoras();
 
 }
--- NaveInicial.java
+++ NaveInicial.java
@@ -4,17 +4,18 @@ public abstract class NaveInicial implements Nave {
         protected int desgaste;
         protected boolean necesitaMantenimiento; //depende del desgaste
         protected Motor motorWarp;
-        protected Tripulacion[] arrayTripulacion;
+        protected Tripulante[] arrayTripulantes;
         protected Mision[] arrayInformes;
+        protected Bitacora[] arrayBitacoras;
 
 
-        public NaveInicial(int combustibleInicial, int energiaInicial, int maxTripulacion, int maxMision){ //la diferencia entre los tipos de nave es por sus configs iniciales
+        public NaveInicial(int combustibleInicial, int energiaInicial, int maxTripulantes, int maxMision){ //la diferencia entre los tipos de nave es por sus configs iniciales
             this.combustible = combustibleInicial;
             this.energia = energiaInicial;
             this.desgaste = 0;
             this.necesitaMantenimiento = false;
             this.motorWarp = new Motor();
-            this.arrayTripulacion = new Tripulacion[maxTripulacion];
+            this.arrayTripulantes = new Tripulante[maxTripulantes];
             this.arrayInformes = new Mision[maxMision];
         }
 
@@ -34,6 +35,65 @@ public abstract class NaveInicial implements Nave {
             this.necesitaMantenimiento = false;
         }
 
+        public void consumeCombustible(int cantCombustible){
+            if (cantCombustible <= this.combustible)
+                this.combustible -= cantCombustible;
+            else
+                this.combustible = 0;
+        }
+
+        public void consumeEnergia(int cantEnergia){
+            if (cantEnergia <= this.energia)
+                this.energia -= cantEnergia;
+            else
+                this.energia = 0;
+        }
+
+        public void incrementaDesgaste(int cantDesgaste){
+            this.desgaste += cantDesgaste;
+            if (this.desgaste >= 80)
+                this.necesitaMantenimiento = true;
+            if (this.desgaste > 100)                //valor tope del desgaste
+                this.desgaste = 100;
+        }
+
+        @Override
+        public double liquidarHaberes(){
+            double total = 0;
+            int n = this.arrayTripulantes.length;
+            int i;
+            for (i = 0; i < n; i++)
+                if(this.arrayTripulantes[i] != null)
+                    total += this.arrayTripulantes[i].getSueldo();
+            return total;
+        }
+
+        public void insertaTripulante(Tripulante tripulante){
+            int i = 0;
+            int n = this.arrayTripulantes.length;
+            while (i < n && this.arrayTripulantes[i] != null)
+                i++;
+            if (i < n)
+                this.arrayTripulantes[i] = tripulante;
+        }
+
+        public void insertaMision(Mision mision){
+            int i = 0;
+            int n = this.arrayInformes.length;
+            while (i < n && this.arrayInformes[i] != null)
+                i++;
+            if (i < n)
+                this.arrayInformes[i] = mision;
+        }
+
+        public void insertaBitacora(Bitacora bitacora){
+            int i = 0;
+            int n = this.arrayBitacoras.length;
+            while (i < n && this.arrayBitacoras[i] != null)
+                i++;
+            if (i < n)
+                this.arrayBitacoras[i] = bitacora;
+        }
         //getters
 
         public int getCombustible(){
@@ -56,12 +116,16 @@ public abstract class NaveInicial implements Nave {
             return this.motorWarp;
         }
 
-        public Tripulacion[] getArrayTripulacion() {
-            return this.arrayTripulacion;
+        public Tripulante[] getArrayTripulantes() {
+            return this.arrayTripulante;
         }
 
         public Mision[] getArrayInformes() {
             return this.arrayInformes;
+        }
+
+        public Bitacora[] getArrayBitacoras() {
+                return this.arrayBitacoras;
         }
 }
 
